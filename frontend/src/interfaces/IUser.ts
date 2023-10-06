a72cb257316866071f0e6472312d66754428a627
supachaicharoen <mr.supachai.00@gmail.com>
2023-10-07 01:03:13 +0700

    Update IUser.ts

--- frontend/src/interfaces/IUser.ts
+++ frontend/src/interfaces/IUser.ts
@@ -1,6 +1,6 @@
 export interface UsersInterface {
-  ID?: number;
-  Title_name?: string;
+  	ID?: number;
+  	Title_name?: string;
 	First_name?: string;
 	Last_name?:  string;
 	User_email?: string;
