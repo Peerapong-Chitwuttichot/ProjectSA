0ebbd1c9e8e0daacec9286d01e7294766cc92998
supachaicharoen <mr.supachai.00@gmail.com>
2023-10-06 16:15:55 +0700

    Create index.tsx

---
+++ frontend/src/pages/customer/create/index.tsx
@@ -0,0 +1,177 @@
+import React from "react";
+import {
+  Space,
+  Button,
+  Col,
+  Row,
+  Divider,
+  Form,
+  Input,
+  Card,
+  message,
+  Upload,
+  DatePicker,
+  Radio,
+} from "antd";
+import { PlusOutlined } from "@ant-design/icons";
+import { UsersInterface } from "../../../interfaces/IUser";
+import { CreateUser } from "../../../services/https";
+import { useNavigate } from "react-router-dom";
+
+const normFile = (e: any) => {
+  if (Array.isArray(e)) {
+    return e;
+  }
+  return e && e.fileList;
+};
+
+function CustomerCreate() {
+  const navigate = useNavigate();
+  const [messageApi, contextHolder] = message.useMessage();
+
+  const onFinish = async (values: UsersInterface) => {
+    try {
+      const res = await CreateUser(values);
+      if (res.status) {
+        messageApi.success("บันทึกข้อมูลสำเร็จ");
+        setTimeout(() => {
+          navigate("/customer");
+        }, 2000);
+      } else {
+        messageApi.error("เกิดข้อผิดพลาดในการบันทึกข้อมูล");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      {contextHolder}
+      <Card>
+        <h2> เพิ่มข้อมูล ผู้ดูแลระบบ</h2>
+        <Divider />
+        <Form
+          name="basic"
+          layout="vertical"
+          onFinish={onFinish}
+          autoComplete="off"
+        >
+          <Row gutter={[16, 16]}>
+            <Col xs={24} sm={24} md={24} lg={24} xl={12}>
+              <Form.Item
+                label="ชื่อจริง"
+                name="FirstName"
+                rules={[
+                  {
+                    required: true,
+                    message: "กรุณากรอกชื่อ !",
+                  },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+            </Col>
+            <Col xs={24} sm={24} md={24} lg={24} xl={12}>
+              <Form.Item
+                label="นามกสุล"
+                name="LastName"
+                rules={[
+                  {
+                    required: true,
+                    message: "กรุณากรอกนามสกุล !",
+                  },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+            </Col>
+            <Col xs={24} sm={24} md={24} lg={24} xl={12}>
+              <Form.Item
+                label="อีเมล"
+                name="Email"
+                rules={[
+                  {
+                    type: "email",
+                    message: "รูปแบบอีเมลไม่ถูกต้อง !",
+                  },
+                  {
+                    required: true,
+                    message: "กรุณากรอกอีเมล !",
+                  },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+            </Col>
+            <Col xs={24} sm={24} md={24} lg={24} xl={12}>
+              <Form.Item
+                label="เบอร์โทรศัพท์"
+                name="Phone"
+                rules={[
+                  {
+                    required: true,
+                    message: "กรุณากรอกเบอร์โทรศัพท์ !",
+                  },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+            </Col>
+            <Col xs={24} sm={24} md={24} lg={24} xl={12}>
+            <Form.Item label="วันเกิด">
+              <DatePicker />
+              </Form.Item>
+            </Col>
+            <Col xs={24} sm={24} md={24} lg={24} xl={24}>
+            <Form.Item label="Radio">
+              <Radio.Group>
+              <Radio value="apple"> Apple </Radio>
+              <Radio value="pear"> Pear </Radio>
+            </Radio.Group>
+            </Form.Item>
+            </Col>
+            <Col xs={24} sm={24} md={24} lg={24} xl={24}>
+              <Form.Item
+                label="อัพโหลดรูปภาพ"
+                name="upload"
+                valuePropName="fileList"
+                getValueFromEvent={normFile}
+              >
+                <Upload
+                  action="/upload.do"
+                  listType="picture-card"
+                >
+                  <div>
+                    <PlusOutlined />
+                    <div style={{ marginTop: 8 }}>Upload</div>
+                  </div>
+                </Upload>
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row justify="end">
+            <Col style={{ marginTop: "40px" }}>
+              <Form.Item>
+                <Space>
+                  <Button htmlType="button" style={{ marginRight: "10px" }}>
+                    ยกเลิก
+                  </Button>
+                  <Button
+                    type="primary"
+                    htmlType="submit"
+                    icon={<PlusOutlined />}
+                  >
+                    ยืนยัน
+                  </Button>
+                </Space>
+              </Form.Item>
+            </Col>
+          </Row>
+        </Form>
+      </Card>
+    </div>
+  );
+}
+
+export default CustomerCreate;
