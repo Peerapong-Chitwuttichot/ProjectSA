7bc84a1c7d68e4420aa94149181dbbe1a2174045
supachaicharoen <mr.supachai.00@gmail.com>
2023-10-06 16:15:37 +0700

    Create IUser.ts

---
+++ frontend/src/interfaces/IUser.ts
@@ -0,0 +1,11 @@
+export interface UsersInterface {
+  ID?: number;
+  Title_name?: string;
+	First_name?: string;
+	Last_name?:  string;
+	User_email?: string;
+	User_pass?:  string;
+	Experience?: string;
+	Skill?:      string;
+	Address?:    string;
+}
