ed6d3a2fd596b41e8960b59bb4f0396625838dbb
supachaicharoen <mr.supachai.00@gmail.com>
2023-10-07 17:05:40 +0700

    Update index.tsx

--- frontend/src/pages/register/user/index.tsx
+++ frontend/src/pages/register/user/index.tsx
@@ -1,12 +1,8 @@
 import React, { useState } from 'react';
 import { 
   Col, 
-  Select, 
   Card, 
-  Statistic, 
-  Space, 
-  Table, 
-  Tag, 
+  Space,  
   Button,
   Form,
   Input,
@@ -14,6 +10,7 @@ import {
   Divider,
   Row,
   Layout,
+  Select,
 } from "antd";
 import {
   AuditOutlined,
@@ -23,34 +20,12 @@ import {
   DownOutlined,
   DownloadOutlined,
 } from "@ant-design/icons";
-import type { ColumnsType } from "antd/es/table";
 import "./style.css";
-import { useNavigate, } from "react-router-dom";
-import { PlusOutlined } from "@ant-design/icons";
+import { Link, useNavigate, } from "react-router-dom";
 import { UsersInterface } from "../../../interfaces/IUser";
 import { CreateUser } from "../../../services/https";
 import type { SizeType } from 'antd/es/config-provider/SizeContext';
 const { TextArea } = Input;
-const { Header, Content, Footer, Sider } = Layout;
-
-interface DataType {
-  key: string;
-  name: string;
-  age: number;
-  address: string;
-  tags: string[];
-}
-
-const handleChange = (value: string) => {
-  console.log(`selected ${value}`);
-};
-
-const normFile = (e: any) => {
-  if (Array.isArray(e)) {
-    return e;
-  }
-  return e && e.fileList;
-};
 
 function RegisterUser() {
   const navigate = useNavigate();
@@ -80,10 +55,11 @@ function RegisterUser() {
     {/* <Header style={{ display: 'flex', alignItems: 'center' }}>
       <div className="demo-logo" />
     </Header> */}
+      {contextHolder}
       <Col xs={24} sm={24} md={24} lg={24} xl={24}>
         <div className='img-back' style={{display: "grid", placeItems: "center", height: "100vh" }}>
           <Space direction="vertical" size="middle">
-            <Card style={{ height: "120px",marginBottom: "-10px",}}>
+            <Card style={{ height: "130px",marginBottom: "-30px",}}>
               <div className="label" style={{ marginLeft: "30px", marginRight: "30px" }}>
                 <p className="div">
                   {/* <span className="space2"></span> */}
@@ -105,7 +81,7 @@ function RegisterUser() {
                   <span className="space3"></span>
                   <span>สกุล</span>
                 </p> */}
-                <Divider />
+                {/* <Divider /> */}
               </div>
             </Card>
             <Form
@@ -118,26 +94,25 @@ function RegisterUser() {
               
                 <div style={{ marginBottom: "10px", marginTop: "20px", marginLeft: "30px", marginRight: "30px" }}>
                   <Row gutter={[16, 0]}>
-                    <Col xs={24} sm={24} md={24} lg={24} xl={4}>
-                      <Form.Item 
-                        className="form-item-wrapper" 
-                        name="title_name" 
+                    <Col  xs={24} sm={24} md={24} lg={24} xl={4}>
+                      <Form.Item
+                        name="title_name"
                         label="คำนำหน้า" 
                         rules={[{ required: true, 
-                                  message: 'กรุณากรอก !' }]}>
-                        <Input placeholder="เช่น นาย นางสาว" />
+                                  message: 'กรุณาเลือก !' }]}>
+                        <Select 
+                          defaultValue="-เลือก-"
+                          style={{ width: 90 }}
+                          optionLabelProp="label"
+                          // onChange={handleChange}
+                          options={[
+                            { value: 'นาย', label: 'นาย' },
+                            { value: 'นาวสาว', label: 'นางสาว' },
+                            { value: 'mr.', label: 'Mr.' },
+                            { value: 'mrs.', label: 'Mrs.'},
+                          ]}
+                        />
                       </Form.Item>
-                      {/* <Select 
-                        defaultValue="โปรดเลือก"
-                        style={{ width: 120 }}
-                        onChange={handleChange}
-                        options={[
-                          { value: 'นาย', label: 'นาย' },
-                          { value: 'นาวสาว', label: 'นางสาว' },
-                          { value: 'mr.', label: 'Mr.' },
-                          { value: 'mrs.', label: 'Mrs.'},
-                        ]}
-                      /> */}
                     </Col>
                     <Col xs={24} sm={24} md={24} lg={24} xl={10}>
                       <Form.Item 
@@ -217,28 +192,111 @@ function RegisterUser() {
                   </Col>
                   <Col xs={24} sm={24} md={24} lg={24} xl={24}>
                     <Form.Item 
-                      className="form-item-wrapper2" 
                       name="address" 
                       label="ที่อยู่ปัจจุบัน" 
                       rules={[{ required: true, 
-                                  message: 'กรุณากรอก !' }]}>
-                      <TextArea rows={2} placeholder="เช่น บ้านหนองอีกึ่ม" />
+                                message: 'กรุณาเลือก !' }]}>
+                      <Select 
+                        defaultValue="--เลือกจังหวัด--"
+                        style={{ width: 610 }}
+                        optionLabelProp="label"
+                        // onChange={handleChange}
+                        options={[
+                          { value: 'กรุงเทพมหานคร', label: 'กรุงเทพมหานคร' },
+                          { value: 'กาญจนบุรี', label: 'กาญจนบุรี' },
+                          { value: 'กาฬสินธุ์', label: 'กาฬสินธุ์' },
+                          { value: 'กำแพงเพชร', label: 'กำแพงเพชร' },
+                          { value: 'ขอนแก่น', label: 'ขอนแก่น' },
+                          { value: 'จันทบุรี', label: 'จันทบุรี' },
+                          { value: 'ฉะเชิงเทรา', label: 'ฉะเชิงเทรา' },
+                          { value: 'ชลบุรี', label: 'ชลบุรี' },
+                          { value: 'ชัยนาท', label: 'ชัยนาท' },
+                          { value: 'ชัยภูมิ', label: 'ชัยภูมิ' },
+                          { value: 'ชุมพร', label: 'ชุมพร' },
+                          { value: 'เชียงราย', label: 'เชียงราย' },
+                          { value: 'เชียงใหม่', label: 'เชียงใหม่' },
+                          { value: 'ตรัง', label: 'ตรัง' },
+                          { value: 'ตราด', label: 'ตราด' },
+                          { value: 'ตาก', label: 'ตาก' },
+                          { value: 'นครนายก', label: 'นครนายก' },
+                          { value: 'นครปฐม', label: 'นครปฐม' },
+                          { value: 'นครพนม', label: 'นครพนม' },
+                          { value: 'นครราชสีมา', label: 'นครราชสีมา' },
+                          { value: 'นครศรีธรรมราช', label: 'นครศรีธรรมราช' },
+                          { value: 'นครสวรรค์', label: 'นครสวรรค์' },
+                          { value: 'นนทบุรี', label: 'นนทบุรี' },
+                          { value: 'นราธิวาส', label: 'นราธิวาส' },
+                          { value: 'น่าน', label: 'น่าน' },
+                          { value: 'บึงกาฬ', label: 'บึงกาฬ' },
+                          { value: 'บุรีรัมย์', label: 'บุรีรัมย์' },
+                          { value: 'ปทุมธานี', label: 'ปทุมธานี' },
+                          { value: 'ประจวบคีรีขันธ์', label: 'ประจวบคีรีขันธ์' },
+                          { value: 'ปราจีนบุรี', label: 'ปราจีนบุรี' },
+                          { value: 'ปัตตานี', label: 'ปัตตานี' },
+                          { value: 'พระนครศรีอยุธยา', label: 'พระนครศรีอยุธยา' },
+                          { value: 'พะเยา', label: 'พะเยา' },
+                          { value: 'พังงา', label: 'พังงา' },
+                          { value: 'พัทลุง', label: 'พัทลุง' },
+                          { value: 'พิจิตร', label: 'พิจิตร' },
+                          { value: 'พิษณุโลก', label: 'พิษณุโลก' },
+                          { value: 'เพชรบุรี', label: 'เพชรบุรี' },
+                          { value: 'เพชรบูรณ์', label: 'เพชรบูรณ์' },
+                          { value: 'แพร่', label: 'แพร่' },
+                          { value: 'ภูเก็ต', label: 'ภูเก็ต' },
+                          { value: 'มหาสารคาม', label: 'มหาสารคาม' },
+                          { value: 'มุกดาหาร', label: 'มุกดาหาร' },
+                          { value: 'แม่ฮ่องสอน', label: 'แม่ฮ่องสอน' },
+                          { value: 'ยโสธร', label: 'ยโสธร' },
+                          { value: 'ยะลา', label: 'ยะลา' },
+                          { value: 'ร้อยเอ็ด', label: 'ร้อยเอ็ด' },
+                          { value: 'ระนอง', label: 'ระนอง' },
+                          { value: 'ระยอง', label: 'ระยอง' },
+                          { value: 'ราชบุรี', label: 'ราชบุรี' },
+                          { value: 'ลพบุรี', label: 'ลพบุรี' },
+                          { value: 'ลำปาง', label: 'ลำปาง' },
+                          { value: 'ลำพูน', label: 'ลำพูน' },
+                          { value: 'เลย', label: 'เลย' },
+                          { value: 'ศรีสะเกษ', label: 'ศรีสะเกษ' },
+                          { value: 'สกลนคร', label: 'สกลนคร' },
+                          { value: 'สงขลา', label: 'สงขลา' },
+                          { value: 'สตูล', label: 'สตูล' },
+                          { value: 'สมุทรปราการ', label: 'สมุทรปราการ' },
+                          { value: 'สมุทรสงคราม', label: 'สมุทรสงคราม' },
+                          { value: 'สมุทรสาคร', label: 'สมุทรสาคร' },
+                          { value: 'สระบุรี', label: 'สระบุรี' },
+                          { value: 'สระแก้ว', label: 'สระแก้ว' },
+                          { value: 'สิงห์บุรี', label: 'สิงห์บุรี' },
+                          { value: 'สุโขทัย', label: 'สุโขทัย' },
+                          { value: 'สุพรรณบุรี', label: 'สุพรรณบุรี' },
+                          { value: 'สุราษฎร์ธานี', label: 'สุราษฎร์ธานี' },
+                          { value: 'สุรินทร์', label: 'สุรินทร์' },
+                          { value: 'หนองคาย', label: 'หนองคาย' },
+                          { value: 'หนองบัวลำภู', label: 'หนองบัวลำภู' },
+                          { value: 'อ่างทอง', label: 'อ่างทอง' },
+                          { value: 'อำนาจเจริญ', label: 'อำนาจเจริญ' },
+                          { value: 'อุดรธานี', label: 'อุดรธานี' },
+                          { value: 'อุตรดิตถ์', label: 'อุตรดิตถ์' },
+                          { value: 'อุทัยธานี', label: 'อุทัยธานี' },
+                          { value: 'อุบลราชธานี', label: 'อุบลราชธานี' },
+                        ]}
+                      />
                     </Form.Item>
                   </Col>
-                </div>
-
-              
+                </div>              
             </Card>
-            <Card style={{ height: "85px",marginTop: "5px",}}>
+            <Card style={{ height: "85px",marginTop: "-15px",}}>
               <Col xs={24} sm={24} md={24} lg={24} xl={24}>
                 <Button htmlType="submit" className='custom-button2' type="primary" size={size}>
                   ลงทะเบียน
                 </Button>
                 <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
                 <span className="text-wrapper-2">หรือ</span>
-                <Button className='custom-button3' type="link">
-                  เข้าสู่ระบบด้วยอีเมล
-                </Button>
+                <span>&nbsp;&nbsp;</span>
+                <Link to='/login/user' className='custom-button3' type="link">
+                  เข้าสู่ระบบ
+                </Link>
+                <span>&nbsp;&nbsp;</span>
+                <span className="text-wrapper-2">ด้วยอีเมล?</span>
               </Col>
             </Card>
             </Form>
