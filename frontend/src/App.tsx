556e570a0fd6ae410856ef2ef55cc6598bb65441
top002200 <142074454+top002200@users.noreply.github.com>
2023-10-11 20:32:43 +0700

    Create App.tsx

---
+++ frontend/src/App.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { UserOutlined, DashboardOutlined } from "@ant-design/icons";
+import type { MenuProps } from "antd";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Navigate,
+} from "react-router-dom";
+
+import { Breadcrumb, Layout, Menu, theme } from "antd";
+import logo from "./assets/logo.png";
+
+
+import Candidatepost from "./pages/register/user";
+import LoginUser from "./pages/login/user";
+
+const { Header, Content, Footer, Sider } = Layout;
+
+const App: React.FC = () => {
+  const page = localStorage.getItem("page");
+  const [collapsed, setCollapsed] = useState(false);
+  const [isRegistered, setIsRegistered] = useState(false); // เพิ่มตัวแปร isRegistered
+
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
+  const setCurrentPage = (val: string) => {
+    localStorage.setItem("page", val);
+  };;
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/register/user" element={<Candidatepost />} />
+        <Route path="/login/user" element={<LoginUser />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
