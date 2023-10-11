df13c5a9f65ea6d87b0e286536b2c9e9c314d134
top002200 <142074454+top002200@users.noreply.github.com>
2023-10-11 20:32:36 +0700

    Create IUser.ts

---
+++ frontend/src/interfaces/IUser.ts
@@ -0,0 +1,9 @@
+export interface UsersInterface {
+  	ID?: number;
+  	Position?: string;
+	Salary?: string;
+	Dsecrition?:  string;
+	// Posttimestamp?: string;
+	Matched?:  string;
+	Address?:		  string;
+}
